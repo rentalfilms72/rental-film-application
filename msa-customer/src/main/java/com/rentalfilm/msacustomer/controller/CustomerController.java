5af0455df1c00285b8b3c474a1cf5ae796fc44c9
ledjizalfra <djizalfra@gmail.com>
2020-11-22 12:16:00 +0100

    UPDATE

--- msa-customer/src/main/java/com/rentalfilm/msacustomer/controller/CustomerController.java
+++ msa-customer/src/main/java/com/rentalfilm/msacustomer/controller/CustomerController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rentalfilm.msacustomer.entity.Customer;
@@ -17,7 +16,6 @@ import com.rentalfilm.msacustomer.exception.CustomerNotFoundException;
 import com.rentalfilm.msacustomer.repository.CustomerRepository;
 
 @RestController
-//@RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
